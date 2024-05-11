ea5f65bf9e4aa9bb89140502dd9761f1290fc88d
Harold_FN_2187 <laryeaharold319@gmail.com>
2024-05-11 13:36:26 +0000

    SAT -1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # installs puppet-lint package
 
 package {'flask':
-    ensure   =>'2.1.0'.
-    name     => 'flask'
-    provider => 'pip3'
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
